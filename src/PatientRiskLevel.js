c1df5649ee0d6efc23b9ecf0934c870f13597e54
davidshin0302 <david.shin0302@gmail.com>
2025-03-25 23:41:30 -0400

    completed displaying patient risk level

--- src/PatientRiskLevel.js
+++ src/PatientRiskLevel.js
@@ -30,6 +30,24 @@ const PatientRiskLevel = () => {
     }
   };
 
+  const fetchPatientRiskLevelAssessment = async (endPoint) => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:8080/assess' + endPoint);
+      if (!response.ok) {
+        throw new Error(`HTTp error, status: ${response.status}`);
+      }
+      const data = await response.text();
+
+      setPatientRiskLevelMessage(data);
+    } catch (error) {
+      console.log('Error while fetching: ', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const handleIdChange = (event) => {
     setSelectedId(event.target.value);
   };
@@ -39,49 +57,51 @@ const PatientRiskLevel = () => {
   };
 
   const handleSearch = () => {
+    let endpoint = '/';
     if (selectedId) {
-      // API call to get patientRiskLevelMessage by id
-      setPatientRiskLevelMessage(
-        'patientRiskLevelMessage  based of id: ' + selectedId
-      );
+      endpoint = endpoint + 'id?patId=' + selectedId;
     } else if (familyName) {
       // API call to get patientRiskLevelMessage by family name
-      setPatientRiskLevelMessage(
-        'patientRiskLevelMessage  based of family name: ' + familyName
-      );
+      endpoint = endpoint + 'familyName?familyName=' + familyName;
     }
+
+    fetchPatientRiskLevelAssessment(endpoint);
   };
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <select className="form-select" onChange={handleIdChange}>
-            <option value="">Select ID</option>
-            {patientRecords.map((patientRecord) => (
-              <option key={patientRecord.patId} value={patientRecord.patId}>
-                {patientRecord.patId}
-              </option>
-            ))}
-          </select>
-          <input
-            type="text"
-            className="form-control mt-2"
-            placeholder="Family Last Name"
-            onChange={handleFamilyNameChange}
-          />
-          <button className="btn btn-primary mt-2" onClick={handleSearch}>
-            Search
-          </button>
+  if (loading) {
+    return <p>loading...</p>;
+  } else {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <select className="form-select" onChange={handleIdChange}>
+              <option value="">Select ID</option>
+              {patientRecords.map((patientRecord) => (
+                <option key={patientRecord.patId} value={patientRecord.patId}>
+                  {patientRecord.patId}
+                </option>
+              ))}
+            </select>
+            <input
+              type="text"
+              className="form-control mt-2"
+              placeholder="Family Last Name"
+              onChange={handleFamilyNameChange}
+            />
+            <button className="btn btn-primary mt-2" onClick={handleSearch}>
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <div className="col">
+            <p>{patientRiskLevelMessage}</p>
+          </div>
         </div>
       </div>
-      <div className="row mt-3">
-        <div className="col">
-          <p>{'Hello'}</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default PatientRiskLevel;
